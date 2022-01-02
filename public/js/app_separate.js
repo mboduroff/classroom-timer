11c8e866f9d3ff91eb5abde54abec490c9f45dc2
mboduroff <martinboduroff@gmail.com>
2022-01-02 16:51:12 +0200

    First commit

---
+++ public/js/app_separate.js
@@ -0,0 +1,732 @@
+const date = new Date();
+const hours = date.getHours();
+const minutes = date.getMinutes();
+
+let hh, mm, clN;
+
+console.log("> start: " + date.toLocaleString('en-uk'));
+
+function checkClass() {
+    const month = date.toLocaleString('en-us', {month: 'long'});
+    const day = date.getDate();
+    const year = date.getFullYear();
+    const dayOfWeek = date.getDay();
+
+    switch (dayOfWeek) {
+        case 1:
+            [hh, mm, clN] = runMondayChecks();
+            break;
+        case 2:
+            [hh, mm, clN] = runTuesdayChecks();
+            break;
+        case 3:
+            [hh, mm, clN] = runWednesdayChecks();
+            break;
+        case 4:
+            [hh, mm, clN] = runThursdayChecks();
+            break;
+        case 5:
+            [hh, mm, clN] = runFridayChecks();
+            break;
+        case 6:
+            [hh, mm, clN] = runSaturdayTests();
+            break;
+        case 0:
+            [hh, mm, clN] = runSundayTests();
+            break;
+
+        default:
+            hh = hours + 1;
+            mm = minutes;
+            clN = 'почивка';
+            break;
+    }
+
+    const eD = new Date(month + ' ' + day + ' ' + year + ' ' + hh + ':' + mm + ':00');
+    return [eD, clN];
+}
+
+const endDate = checkClass()[0];
+const className = checkClass()[1];
+
+console.log("> end: " + endDate.toLocaleString('en-uk'));
+
+function runMondayChecks() {
+    switch (hours) {
+        case 7:
+            hh = '08';
+            mm = '45';
+            clN = 'Математика';
+            break;
+        case 8:
+            if (minutes < 45) {
+                hh = '08';
+                mm = '45';
+                clN = 'Математика';
+            } else if (minutes < 55) {
+                hh = '08';
+                mm = '55';
+                clN = 'Междучасие';
+            } else {
+                hh = '09';
+                mm = '40';
+                clN = 'Математика';
+            }
+            break;
+        case 9:
+            if (minutes < 40) {
+                hh = '09';
+                mm = '40';
+                clN = 'Математика';
+            } else if (minutes < 50) {
+                hh = '09';
+                mm = '50';
+                clN = 'Междучасие';
+            } else {
+                hh = '10';
+                mm = '35';
+                clN = 'Информатика';
+            }
+            break;
+        case 10:
+            if (minutes < 35) {
+                hh = '10';
+                mm = '35';
+                clN = 'Информатика';
+            } else if (minutes < 45) {
+                hh = '10';
+                mm = '45';
+                clN = 'Междучасие';
+            } else {
+                hh = '11';
+                mm = '30';
+                clN = 'Информатика';
+            }
+            break;
+        case 11:
+            if (minutes < 30) {
+                hh = '11';
+                mm = '30';
+                clN = 'Информатика';
+            } else if (minutes < 50) {
+                hh = '11';
+                mm = '50';
+                clN = 'Голямо междучасие';
+            } else {
+                hh = '12';
+                mm = '35';
+                clN = 'English';
+            }
+            break;
+        case 12:
+            if (minutes < 35) {
+                hh = '12';
+                mm = '35';
+                clN = 'English';
+            } else if (minutes < 45) {
+                hh = '12';
+                mm = '45';
+                clN = 'Междучасие';
+            } else {
+                hh = '13';
+                mm = '30';
+                clN = 'Deutsch';
+            }
+            break;
+        case 13:
+            if (minutes < 30) {
+                hh = '13';
+                mm = '30';
+                clN = 'Deutsch';
+            } else if (minutes < 35) {
+                hh = '13';
+                mm = '35';
+                clN = 'Междучасие';
+            } else {
+                hh = '14';
+                mm = '20';
+                clN = 'Литература';
+            }
+            break;
+        case 14:
+            if (minutes < 20) {
+                hh = '14';
+                mm = '20';
+                clN = 'Литература';
+            } else {
+                hh = '14';
+                mm = '20';
+                clN = 'учебния ден';
+            }
+            break;
+        default:
+            hh = hours + 1;
+            mm = minutes;
+            clN = 'Почивка';
+            break;
+    }
+    return [hh, mm, clN];
+}
+
+function runTuesdayChecks() {
+    switch (hours) {
+        case 7:
+            hh = '08';
+            mm = '45';
+            clN = 'Час на класа';
+            break;
+        case 8:
+            if (minutes < 45) {
+                hh = '08';
+                mm = '45';
+                clN = 'Час на класа';
+            } else if (minutes < 55) {
+                hh = '08';
+                mm = '55';
+                clN = 'Междучасие';
+            } else {
+                hh = '09';
+                mm = '40';
+                clN = 'ФВС';
+            }
+            break;
+        case 9:
+            if (minutes < 40) {
+                hh = '09';
+                mm = '40';
+                clN = 'ФВС';
+            } else if (minutes < 50) {
+                hh = '09';
+                mm = '50';
+                clN = 'Междучасие';
+            } else {
+                hh = '10';
+                mm = '35';
+                clN = 'English';
+            }
+            break;
+        case 10:
+            if (minutes < 35) {
+                hh = '10';
+                mm = '35';
+                clN = 'English';
+            } else if (minutes < 45) {
+                hh = '10';
+                mm = '45';
+                clN = 'Междучасие';
+            } else {
+                hh = '11';
+                mm = '30';
+                clN = 'Математика';
+            }
+            break;
+        case 11:
+            if (minutes < 30) {
+                hh = '11';
+                mm = '30';
+                clN = 'Математика';
+            } else if (minutes < 50) {
+                hh = '11';
+                mm = '50';
+                clN = 'Голямо междучасие';
+            } else {
+                hh = '12';
+                mm = '35';
+                clN = 'Математика';
+            }
+            break;
+        case 12:
+            if (minutes < 35) {
+                hh = '12';
+                mm = '35';
+                clN = 'Математика';
+            } else if (minutes < 45) {
+                hh = '12';
+                mm = '45';
+                clN = 'Междучасие';
+            } else {
+                hh = '13';
+                mm = '30';
+                clN = 'Гражданско образование';
+            }
+            break;
+        case 13:
+            if (minutes < 30) {
+                hh = '13';
+                mm = '30';
+                clN = 'Гражданско образование';
+            } else if (minutes < 35) {
+                hh = '13';
+                mm = '35';
+                clN = 'Междучасие';
+            } else {
+                hh = '14';
+                mm = '20';
+                clN = 'Литература';
+            }
+            break;
+        case 14:
+            if (minutes < 20) {
+                hh = '14';
+                mm = '20';
+                clN = 'Литература';
+            } else {
+                hh = '14';
+                mm = '20';
+                clN = 'учебния ден';
+            }
+            break;
+        default:
+            hh = hours + 1;
+            mm = minutes;
+            clN = 'Почивка';
+            break;
+    }
+
+    return [hh, mm, clN];
+}
+
+function runWednesdayChecks() {
+    switch (hours) {
+        case 7:
+            hh = '08';
+            mm = '45';
+            clN = 'Математика';
+            break;
+        case 8:
+            if (minutes < 45) {
+                hh = '08';
+                mm = '45';
+                clN = 'Математика';
+            } else if (minutes < 55) {
+                hh = '08';
+                mm = '55';
+                clN = 'Междучасие';
+            } else {
+                hh = '09';
+                mm = '40';
+                clN = 'Математика';
+            }
+            break;
+        case 9:
+            if (minutes < 40) {
+                hh = '09';
+                mm = '40';
+                clN = 'Математика';
+            } else if (minutes < 50) {
+                hh = '09';
+                mm = '50';
+                clN = 'Междучасие';
+            } else {
+                hh = '11';
+                mm = '20';
+                clN = 'English';
+            }
+            break;
+        case 10:
+            if (minutes < 35) {
+                hh = '10';
+                mm = '35';
+                clN = 'English';
+            } else if (minutes < 45) {
+                hh = '10';
+                mm = '45';
+                clN = 'Междучасие';
+            } else {
+                hh = '11';
+                mm = '30';
+                clN = 'English';
+            }
+            break;
+        case 11:
+            if (minutes < 30) {
+                hh = '11';
+                mm = '30';
+                clN = 'English';
+            } else if (minutes < 50) {
+                hh = '11';
+                mm = '50';
+                clN = 'Голямо междучасие';
+            } else {
+                hh = '13';
+                mm = '20';
+                clN = 'Информатика';
+            }
+            break;
+        case 12:
+            if (minutes < 35) {
+                hh = '12';
+                mm = '35';
+                clN = 'Информатика';
+            } else if (minutes < 45) {
+                hh = '12';
+                mm = '45';
+                clN = 'Междучасие';
+            } else {
+                hh = '13';
+                mm = '30';
+                clN = 'Информатика';
+            }
+            break;
+        case 13:
+            if (minutes < 30) {
+                hh = '13';
+                mm = '30';
+                clN = 'Информатика';
+            } else {
+                hh = '13';
+                mm = '30';
+                clN = 'учебния ден';
+            }
+            break;
+        default:
+            hh = hours + 1;
+            mm = minutes;
+            clN = 'Почивка';
+            break;
+    }
+
+    return [hh, mm, clN];
+}
+
+function runThursdayChecks() {
+    switch (hours) {
+        case 7:
+            hh = '09';
+            mm = '30';
+            clN = 'English';
+            break;
+        case 8:
+            hh = '09';
+            mm = '30';
+            clN = 'English';
+            break;
+        case 9:
+            if (minutes < 30) {
+                hh = '09';
+                mm = '30';
+                clN = 'English';
+            } else if (minutes < 50) {
+                hh = '09';
+                mm = '50';
+                clN = 'Междучасие';
+            } else {
+                hh = '10';
+                mm = '35';
+                clN = 'Deutsch';
+            }
+            break;
+        case 10:
+            if (minutes < 35) {
+                hh = '10';
+                mm = '35';
+                clN = 'Deutsch';
+            } else if (minutes < 45) {
+                hh = '10';
+                mm = '45';
+                clN = 'Междучасие';
+            } else {
+                hh = '11';
+                mm = '30';
+                clN = 'Български език';
+            }
+            break;
+        case 11:
+            if (minutes < 30) {
+                hh = '11';
+                mm = '30';
+                clN = 'Български език';
+            } else if (minutes < 50) {
+                hh = '11';
+                mm = '50';
+                clN = 'Голямо междучасие';
+            } else {
+                hh = '12';
+                mm = '35';
+                clN = 'Математика';
+            }
+            break;
+        case 12:
+            if (minutes < 35) {
+                hh = '12';
+                mm = '35';
+                clN = 'Математика';
+            } else if (minutes < 45) {
+                hh = '12';
+                mm = '45';
+                clN = 'Междучасие';
+            } else {
+                hh = '13';
+                mm = '30';
+                clN = 'ФВС';
+            }
+            break;
+        case 13:
+            if (minutes < 30) {
+                hh = '13';
+                mm = '30';
+                clN = 'ФВС';
+            } else {
+                hh = '13';
+                mm = '30';
+                clN = 'учебния ден';
+            }
+            break;
+        default:
+            hh = hours + 1;
+            mm = minutes;
+            clN = 'Почивка';
+            break;
+    }
+
+    return [hh, mm, clN];
+}
+
+function runFridayChecks() {
+    switch (hours) {
+        case 7:
+            hh = '08';
+            mm = '45';
+            clN = 'Математика';
+            break;
+        case 8:
+            if (minutes < 45) {
+                hh = '08';
+                mm = '45';
+                clN = 'Математика';
+            } else if (minutes < 55) {
+                hh = '08';
+                mm = '55';
+                clN = 'Междучасие';
+            } else {
+                hh = '09';
+                mm = '40';
+                clN = 'Математика';
+            }
+            break;
+        case 9:
+            if (minutes < 40) {
+                hh = '09';
+                mm = '40';
+                clN = 'Математика';
+            } else if (minutes < 50) {
+                hh = '09';
+                mm = '50';
+                clN = 'Междучасие';
+            } else {
+                hh = '10';
+                mm = '35';
+                clN = 'Информатика';
+            }
+            break;
+        case 10:
+            if (minutes < 35) {
+                hh = '10';
+                mm = '35';
+                clN = 'Информатика';
+            } else if (minutes < 45) {
+                hh = '10';
+                mm = '45';
+                clN = 'Междучасие';
+            } else {
+                hh = '11';
+                mm = '30';
+                clN = 'Информатика';
+            }
+            break;
+        case 11:
+            if (minutes < 30) {
+                hh = '11';
+                mm = '30';
+                clN = 'Информатика';
+            } else if (minutes < 50) {
+                hh = '11';
+                mm = '50';
+                clN = 'Голямо междучасие';
+            } else {
+                hh = '12';
+                mm = '35';
+                clN = 'English';
+            }
+            break;
+        case 12:
+            if (minutes < 35) {
+                hh = '12';
+                mm = '35';
+                clN = 'English';
+            } else if (minutes < 45) {
+                hh = '12';
+                mm = '45';
+                clN = 'Междучасие';
+            } else {
+                hh = '13';
+                mm = '30';
+                clN = 'English';
+            }
+            break;
+        case 13:
+            if (minutes < 30) {
+                hh = '13';
+                mm = '30';
+                clN = 'English';
+            } else if (minutes < 35) {
+                hh = '13';
+                mm = '35';
+                clN = 'Междучасие';
+            } else {
+                hh = '14';
+                mm = '20';
+                clN = 'БЕЛ-РП';
+            }
+            break;
+        case 14:
+            if (minutes < 20) {
+                hh = '14';
+                mm = '20';
+                clN = 'БЕЛ-РП';
+            } else {
+                hh = '14';
+                mm = '20';
+                clN = 'учебния ден';
+            }
+            break;
+        default:
+            hh = hours + 1;
+            mm = minutes;
+            clN = 'Почивка';
+            break;
+    }
+
+    return [hh, mm, clN];
+}
+
+function runSaturdayTests() {
+    switch (hours) {
+        default:
+            hh = hours + 1;
+            mm = minutes;
+            clN = 'Тестване';
+            break;
+    }
+
+    return [hh, mm, clN];
+}
+
+function runSundayTests() {
+    if (minutes < 15) {
+        hh = hours + '';
+        mm = '15';
+        clN = 'Междучасие';
+    } else if (minutes < 30) {
+        hh = hours + '';
+        mm = '30';
+        clN = 'English';
+    } else if (minutes < 45) {
+        hh = hours + '';
+        mm = '45';
+        clN = 'Deutsch';
+    } else if (minutes < 58) {
+        hh = hours + '';
+        mm = '58';
+        clN = 'Математика';
+    } else {
+        hh = hours + 1 + '';
+        mm = '35';
+        clN = 'Тестване';
+    }
+
+    return [hh, mm, clN];
+}
+
+
+// select elements
+const app = document.querySelector('.countdown-timer');
+const message = document.querySelector('.message');
+const heading = document.querySelector('h1');
+
+
+const format = (t) => {
+    return t < 10 ? '0' + t : t;
+};
+
+const render = (time) => {
+    if (time.hours >= 1) {
+        app.innerHTML = `
+        <div class="count-down" id="timer">
+            <div class="timer">
+                <h2 class="hours" >${format(time.hours)}</h2>
+                <small>Часа</small>
+            </div>
+            <div class="timer">
+                <h2 class="minutes">${format(time.minutes)}</h2>
+                <small>Минути</small>
+            </div>
+            <div class="timer">
+                <h2 class="seconds">${format(time.seconds)}</h2>
+                <small>Секунди</small>
+            </div>
+        </div>
+        `;
+    } else if (time.minutes >= 1) {
+        app.innerHTML = `
+        <div class="count-down">
+            <div class="timer">
+                <h2 class="minutes">${format(time.minutes)}</h2>
+                <small>Минути</small>
+            </div>
+            <div class="timer">
+                <h2 class="seconds">${format(time.seconds)}</h2>
+                <small>Секунди</small>
+            </div>
+        </div>
+        `;
+    } else {
+        app.innerHTML = `
+        <div class="count-down">
+            <div class="timer">
+                <h2 class="seconds">${format(time.seconds)}</h2>
+                <small>Секунди</small>
+            </div>
+        </div>
+        `;
+    }
+};
+
+
+function showMessage() {
+    message.innerHTML = "Край на " + className;
+    app.innerHTML = '';
+    heading.style.display = 'none';
+}
+
+const hideMessage = () => {
+    message.innerHTML = '';
+    heading.style.display = 'block';
+};
+
+function complete() {
+    showMessage();
+    playAudio();
+    // restart the countdown after showing the
+    // greeting message for (--a day--) 9 seconds  ()
+    setTimeout(() => {
+        hideMessage();
+        countdownTimer.setExpiredDate(endDate);
+        window.location.reload();
+    }, 9000); // ms
+}
+
+function playAudio() {
+    const audio = new Audio('../audio/bell.wav');
+    audio.play().then(() => {
+        console.log("bell rung; reloading page");
+    });
+}
+
+const countdownTimer = new CountDown(
+    checkClass()[0],
+    render,
+    complete
+);
+
+console.log("> class name: " + className);
---
+++ public/js/bel_class_counter.js
@@ -0,0 +1,68 @@
+function getDaysOfBelLeft() {
+    /* Two working days and a Sunday (not working day) */
+    const holidays = ['2021-10-05', '2021-10-06', '2021-10-13', '2021-10-20',
+        '2021-10-27', '2021-10-30', '2021-10-31', '2021-11-01', '2021-11-03',
+        '2021-11-10', '2021-11-15', '2021-11-17', '2021-11-24', '2021-12-01',
+        '2021-12-08', '2021-12-15', '2021-12-22', '2021-12-24', '2021-12-25',
+        '2021-12-26', '2021-12-27', '2021-12-28', '2021-12-29', '2021-12-30',
+        '2021-12-31', '2022-01-01', '2022-01-02', '2022-01-05', '2022-01-12',
+        '2022-01-19', '2022-01-26', '2022-02-01', '2022-02-02', '2022-02-09',
+        '2022-02-16', '2022-02-23', '2022-03-01', '2022-03-02', '2022-03-03',
+        '2022-03-09', '2022-03-16', '2022-03-23', '2022-03-30', '2022-04-06',
+        '2022-04-07', '2022-04-08', '2022-04-09', '2022-04-10', '2022-04-13',
+        '2022-04-20', '2022-04-22', '2022-04-25', '2022-04-27', '2022-05-02',
+        '2022-05-04', '2022-05-06', '2022-05-11'];
+    const startDate = new Date();
+    const endDate = new Date("2022-05-12"); // end of school year
+    
+// Validate input
+    if (endDate <= startDate) {
+        return 0;
+    }
+
+// Calculate days between dates
+    const millisecondsPerDay = 86400000; // Day in milliseconds
+    startDate.setHours(0, 0, 0, 1);  // Start just after midnight
+    endDate.setHours(23, 59, 59, 999);  // End just before midnight
+    const diff = endDate - startDate;  // Milliseconds between datetime objects
+    let days = Math.ceil(diff / millisecondsPerDay);
+
+    // Subtract two weekend days for every week in between
+    const weeks = Math.floor(days / 7);
+    days -= weeks * 2;
+
+    // Handle special cases
+    const startDay = startDate.getDay();
+    const endDay = endDate.getDay();
+
+    // Remove weekend not previously removed.
+    if (startDay - endDay > 1) {
+        days -= 2;
+    }
+    // Remove start day if span starts on Sunday but ends before Saturday
+    if (startDay === 0 && endDay !== 6) {
+        days--;
+    }
+
+    // Remove end day if span ends on Saturday but starts after Sunday
+    if (endDay === 6 && startDay !== 0) {
+        days--;
+    }
+
+    /* Here is the code */
+    holidays.forEach(day => {
+        if ((day >= startDate.toISOString()) && (day <= endDate.toISOString())) {
+            /* If it is not Saturday (6) or Sunday (0), subtract it */ // but also Wednesday
+            if ((parseDate(day).getDay() % 6) !== 0) {
+                days--;
+            }
+        }
+    });
+    return days;
+}
+
+function parseDate(input) {
+    const parts = input.match(/(\d+)/g);
+    return new Date(parts[0], parts[1] - 1, parts[2]);
+}
+
